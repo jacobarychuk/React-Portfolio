5ff1d5cb33e4174f30709203b0dc235b06d34873
Jacob Arychuk <156041293+jacobarychuk@users.noreply.github.com>
2025-01-18 23:09:36 -0800

    feat(tags): update styles for better alignment

--- react-portfolio/src/components/Tag.js
+++ react-portfolio/src/components/Tag.js
@@ -1,13 +1,15 @@
 function Tag({ label, color }) {
   const tagStyle = {
     display: 'inline-block',
-    border: `2px solid ${color}`,
+    border: `0.125rem solid ${color}`,
     borderRadius: '9999px',
     padding: '0.25rem 0.75rem',
     backgroundColor: 'white',
     fontSize: '1rem',
     fontWeight: 'bold',
     color: color,
+    height: '2rem',
+    boxSizing: 'border-box',
   };
 
   return <div style={tagStyle}>{label}</div>;
